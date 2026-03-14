28c431704fe1836b009034dcf0375a23a35faab5
Walace Ramos <contatowalace.dev@gmail.com>
2026-03-14 10:03:00 -0300

    adicionando automação para artigos

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // Documentação: https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
+  
   vite: {
     plugins: [tailwindcss()]
   },
